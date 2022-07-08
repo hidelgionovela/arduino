c21191f532dea72de1de8b7bf32615355dc94755
Hidélgio Novela <hidelgionovela@gmail.com>
2022-07-08 14:55:03 +0200

    chore(display): Completando os números

--- display7Segimentos/display/display.ino
+++ display7Segimentos/display/display.ino
@@ -1,10 +1,18 @@
 #include <Adafruit_MCP23008.h>
 // instanciando o display de sete seguimentos
 Adafruit_MCP23008 seteSeguimentos; 
-uint8_t numeros[3] = {
+uint8_t numeros[10] = {
    B00111111,
    B00000110,
-   B01011011
+   B01011011,
+   B01001111,
+   B01100110,
+   B01101101,
+   B01111100,
+   B00000111,
+   B01111111,
+   B01100111
+   
    
    };
 
@@ -12,12 +20,12 @@ uint8_t numeros[3] = {
 void setup() {
   
   seteSeguimentos.begin(0x4);
-  for(int x = 0; x<8; x++ ){
+  for(int x = 0; x<11; x++ ){
     seteSeguimentos.pinMode(x,OUTPUT);}
 }
 
 void loop() {
-    for(int x = 0; x<3; x++ ){
+    for(int x = 0; x<11; x++ ){
     seteSeguimentos.writeGPIO(numeros[x]);
     delay(250);
 }
